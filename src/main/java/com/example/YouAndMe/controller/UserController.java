6f83765d9ee8075eff5d59c7141e0d3246b53144
Le Van Dung <flynn@192.168.2.1>
2024-04-16 16:51:33 +0700

    fix jenkins

--- src/main/java/com/example/YouAndMe/controller/UserController.java
+++ src/main/java/com/example/YouAndMe/controller/UserController.java
@@ -3,15 +3,12 @@ package com.example.YouAndMe.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.YouAndMe.entity.User;
 import com.example.YouAndMe.service.UserService;
 
 import jakarta.annotation.Nullable;
-import jakarta.validation.Valid;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
