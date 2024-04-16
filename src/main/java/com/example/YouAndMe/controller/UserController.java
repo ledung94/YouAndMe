fecdf566aaba2c94456c40891a0d63d51afceb81
Le Van Dung <flynn@192.168.2.1>
2024-04-16 16:59:56 +0700

    fix jenkins

--- src/main/java/com/example/YouAndMe/controller/UserController.java
+++ src/main/java/com/example/YouAndMe/controller/UserController.java
@@ -13,10 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
 @RestController
+@RequestMapping("api/public")
 public class UserController {
     @Autowired
     UserService accountService;
